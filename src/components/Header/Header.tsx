9a2af833f1220a86eaca8005ef43c333cd6470c6
Tiago Gomes <tidgomes@gmail.com>
2024-12-04 11:26:44 -0300

    feat: import google font for the books page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import classes from "./Header.module.css";
 const Header: React.FC = () => {
   return (
     <header className={classes.header}>
-      <p>The header</p>
+      <p className={classes.headerTitle}>Books</p>
     </header>
   );
 };
