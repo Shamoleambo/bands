b1f1583aa9d9ec9ab5bec4db19db32484c61b8cf
Tiago Gomes <tidgomes@gmail.com>
2024-12-05 16:12:28 -0300

    feat: add backdrop to the modal component

--- src/components/AuthorModal/AuthorModal.tsx
+++ src/components/AuthorModal/AuthorModal.tsx
@@ -8,24 +8,27 @@ const AuthorModal: React.FC<{
   onClose: () => void;
 }> = ({ name, info, onClose }) => {
   const modalContent = (
-    <div className={classes.authorContainer}>
-      <div className={classes.imageContainer}>
-        <img alt={name} src={info.picture} />
+    <>
+      <div onClick={onClose} className={classes.backdrop}></div>
+      <div className={classes.authorContainer}>
+        <div className={classes.imageContainer}>
+          <img alt={name} src={info.picture} />
+        </div>
+        <div className={classes.generalInfo}>
+          <button className={classes.button} onClick={onClose}>
+            X
+          </button>
+          <h2 className={classes.authorName}>{name}</h2>
+          <p>{info.about}</p>
+          <h4>Also Known for:</h4>
+          <ul>
+            {info.otherBooks.map((book) => (
+              <li key={book}>{book}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className={classes.generalInfo}>
-        <button className={classes.button} onClick={onClose}>
-          X
-        </button>
-        <h2 className={classes.authorName}>{name}</h2>
-        <p>{info.about}</p>
-        <h4>Also Known for:</h4>
-        <ul>
-          {info.otherBooks.map((book) => (
-            <li key={book}>{book}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </>
   );
 
   return createPortal(modalContent, document.getElementById("portal")!);
