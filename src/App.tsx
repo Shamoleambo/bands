15a4e4e2a5b91e40c47a53ebe191255ce2169295
Tiago Gomes <tidgomes@gmail.com>
2024-12-05 10:03:43 -0300

    feat: display the BooksContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
-import "./App.css";
 import Footer from "./components/Footer/Footer";
+import myData from "./data/books.json";
+import "./App.css";
+import BooksContainer from "./components/BooksContainer/BooksContainer";
 
 function App() {
+  const [books, setBooks] = useState(myData);
+
   return (
     <div className="app">
       <Header />
+      <main>
+        <BooksContainer books={books} />
+      </main>
       <Footer />
     </div>
   );
