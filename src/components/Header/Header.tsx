ee1aba62aec22d024fe45556f21beb04ec8a512a
Tiago Gomes <tidgomes@gmail.com>
2024-12-09 15:40:13 -0300

    feat: make the page title a navlink to home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
+import { NavLink } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import classes from "./Header.module.css";
 
 const Header: React.FC = () => {
   return (
     <header className={classes.header}>
-      <p className={classes.headerTitle}>Books</p>
+      <NavLink to="/">
+        <p className={classes.headerTitle}>Books</p>
+      </NavLink>
       <NavBar />
     </header>
   );
